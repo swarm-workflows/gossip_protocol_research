adf5f07dcfd4fa0f54af4047de20042ff8c82b33
shixun404 <shixun404@gmail.com>
2024-04-09 09:40:30 +0000

    update

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import subprocess
 import socket
+import time
 # Define the IP address and base port
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
-s.close()
 ip_address = s.getsockname()[0]
+s.close()
 sd_address = "192.168.100.228"
 base_port = 1234
 log_directory = "./test_log"
@@ -21,9 +22,9 @@ with open(f"{log_directory}/rapid.{base_port}", "w") as logfile:
     subprocess.Popen([
         "java", "-jar", "examples/target/standalone-agent.jar",
         "--listenAddress", f"{ip_address}:{base_port}",
-        "--seedAddress", f"{ip_address}:{base_port}"
+        "--seedAddress", f"{sd_address}:{base_port}"
     ], stdout=logfile, stderr=subprocess.STDOUT)
-
+time.sleep(5)
 # Loop to start agents on ports from start_port to end_port
 for port in range(start_port, end_port):
     with open(f"{log_directory}/rapid.{port}", "w") as logfile:
