d47564efafcbb94ef296dd728d45fe26e4872242
shixun404 <shixun404@gmail.com>
2024-04-16 13:27:19 +0000

    update

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 #! /bin/bash 
 IP=192.168.100.201
 initial_IP=192.168.100.228
-java -jar examples/target/standalone-agent.jar --listenAddress $IP:1234 --seedAddress $IP:1234 &> ./test_log/rapid.1234 &
+java -jar examples/target/standalone-agent.jar --listenAddress $IP:1234 --seedAddress $initial_IP:1234 &> ./test_log/rapid.1234 &
 for each in `seq 1235 1283`;
 do
     java -jar examples/target/standalone-agent.jar --listenAddress $IP:$each --seedAddress $IP:1234 &> ./test_log/rapid.$each &
