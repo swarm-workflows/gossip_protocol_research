3c86b3584c9fb4cce2d6b819d40eab4d354202bf
shixun404 <shixun404@gmail.com>
2024-08-22 22:12:35 +0000

    update

---
+++ test/sender.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    struct sockaddr_in serv_addr;
+    int sock = 0;
+    char *message = "Hello from client";
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    send(sock, message, strlen(message), 0);
+    printf("Hello message sent\n");
+
+    close(sock);
+    return 0;
+}
