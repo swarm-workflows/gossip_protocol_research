412edc157dfc19d8fa84cdfb30b4ca61644e30bb
Chameleon Cloud User <shixun404@gmail.com>
2025-02-01 06:24:40 +0000

    update

--- examples/src/main/java/com/vrg/standalone/ClusterTest.java
+++ examples/src/main/java/com/vrg/standalone/ClusterTest.java
@@ -158,7 +158,6 @@ import static org.junit.Assert.assertTrue;
             catch (Exception e) {
             System.err.println("Failed to connect to the cluster at " + baseIP + ":" + (port - 1));
             }
-            return;
         }
         
         waitForConnections();
