ea3fc0c138c010b2ab412d6fc4f524371fc9510a
Chameleon Cloud User <shixun404@gmail.com>
2025-02-03 07:49:06 +0000

    update

--- examples/src/main/java/com/vrg/standalone/ClusterTest.java
+++ examples/src/main/java/com/vrg/standalone/ClusterTest.java
@@ -331,7 +331,9 @@ import static org.junit.Assert.assertTrue;
         //   ", Endpoint: " + seedEndpoint);
         //     }
           if(nodeId == 0){
-            final UUID TEST_MESSAGE_UUID = UUID.fromString("00000000-0000-0000-0000-000000000001");
+            // final UUID TEST_MESSAGE_UUID = UUID.fromString("00000000-0000-0000-0000-000000000001");
+            final UUID TEST_MESSAGE_UUID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
+            
             MessageId testMessageId = Utils.messageIdFromUUID(TEST_MESSAGE_UUID);
             System.out.println("Broadcast Start at " + System.currentTimeMillis()  +
              ", Endpoint: " + seedEndpoint);
