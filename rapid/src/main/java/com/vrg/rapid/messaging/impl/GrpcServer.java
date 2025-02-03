7a8e4ba6513f377b4a704763b46cbe88cfe131e9
Chameleon Cloud User <shixun404@gmail.com>
2025-02-03 08:24:02 +0000

    update

--- rapid/src/main/java/com/vrg/rapid/messaging/impl/GrpcServer.java
+++ rapid/src/main/java/com/vrg/rapid/messaging/impl/GrpcServer.java
@@ -116,7 +116,7 @@ public class GrpcServer extends MembershipServiceGrpc.MembershipServiceImplBase 
                 System.out.println("Server receives PHASE2BMESSAGE: " + System.currentTimeMillis()  + ", Endpoint: " + address); 
             }
             else{
-                System.out.println("messageId: " + messageId + "testMessageId: " + testMessageId);
+                System.out.println("messageId: " + rapidRequest.getMessageId() + " messageId_string: " + messageId + " testMessageId: " + testMessageId);
             }
             if (messageCache.getIfPresent(messageId) != null) {
                 return;
