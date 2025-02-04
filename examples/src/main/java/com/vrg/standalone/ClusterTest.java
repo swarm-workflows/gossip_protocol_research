e6ffba8a2fb27537d1a9be805ea9cf18ffb8f706
Chameleon Cloud User <shixun404@gmail.com>
2025-02-04 04:56:55 +0000

    update

--- examples/src/main/java/com/vrg/standalone/ClusterTest.java
+++ examples/src/main/java/com/vrg/standalone/ClusterTest.java
@@ -324,7 +324,7 @@ import static org.junit.Assert.assertTrue;
             final Endpoint seedEndpoint = Utils.hostFromParts(baseIP, basePort);
             if(nodeId == 0) createCluster(numNodes, seedEndpoint);
             else {
-                Thread.sleep(5000 + 10000 * (long)(nodeId - 1));
+                Thread.sleep(5000 + 20000 * (long)((nodeId - 1) / 2));
                 extendClusterWithRetry(numNodes, seedEndpoint);
             }
             System.out.println("Wait for targetNodes: " + targetNodes);
