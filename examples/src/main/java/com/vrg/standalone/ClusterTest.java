c1d1e1467343fac7e565f7804edec6e70f07fb01
Chameleon Cloud User <shixun404@gmail.com>
2025-02-04 04:16:42 +0000

    update

--- examples/src/main/java/com/vrg/standalone/ClusterTest.java
+++ examples/src/main/java/com/vrg/standalone/ClusterTest.java
@@ -323,7 +323,10 @@ import static org.junit.Assert.assertTrue;
      public void run(final int numNodes) throws IOException, InterruptedException {
             final Endpoint seedEndpoint = Utils.hostFromParts(baseIP, basePort);
             if(nodeId == 0) createCluster(numNodes, seedEndpoint);
-            else extendClusterWithRetry(numNodes, seedEndpoint);
+            else {
+                Thread.sleep(3000 * (nodeId - 1));
+                extendClusterWithRetry(numNodes, seedEndpoint);
+            }
             System.out.println("Wait for targetNodes: " + targetNodes);
             waitAndVerifyAgreement(targetNodes, 10, 1000);
             System.out.println("TargetNodes=" + targetNodes + " have joined the cluster.");
