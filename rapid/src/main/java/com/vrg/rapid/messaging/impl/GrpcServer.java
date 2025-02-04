0a78cf3458d1a1ae63e8e993799c5bd4c200c17d
Chameleon Cloud User <shixun404@gmail.com>
2025-02-04 07:32:50 +0000

    update

--- rapid/src/main/java/com/vrg/rapid/messaging/impl/GrpcServer.java
+++ rapid/src/main/java/com/vrg/rapid/messaging/impl/GrpcServer.java
@@ -83,8 +83,8 @@ public class GrpcServer extends MembershipServiceGrpc.MembershipServiceImplBase 
         this.useInProcessServer = useInProcessTransport;
         // Initialize the cache with a maximum size and expiration time
         this.messageCache = Caffeine.newBuilder()
-        .expireAfterWrite(1000, TimeUnit.SECONDS)
-        .maximumSize(1000)
+        .expireAfterWrite(5000, TimeUnit.MILLISECONDS)
+        .maximumSize(5000)
         .build();
     }
 
