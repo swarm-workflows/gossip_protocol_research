d78c3261ca9749beb612d4e17e5a6a1a98a45c59
Chameleon Cloud User <shixun404@gmail.com>
2025-02-05 18:07:19 +0000

    update

--- examples/src/main/java/com/vrg/standalone/ClusterTest.java
+++ examples/src/main/java/com/vrg/standalone/ClusterTest.java
@@ -354,7 +354,7 @@ import static org.junit.Assert.assertTrue;
             long startTime = System.nanoTime(); // 开始计时
             if(nodeId == 0){
                 Endpoint leavingEndpoint = Utils.hostFromParts(myIP, basePort + 1);
-                for (int i = 0; i < 10; i++) {
+                for (int i = 0; i < 5; i++) {
                     final Cluster cluster = instances.remove(leavingEndpoint);
                     cluster.shutdown();
                     System.out.println("Node " + leavingEndpoint + " shutdown.");
