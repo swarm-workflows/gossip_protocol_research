1fceb44368f0e991768d45ac364903a4bce93f52
Chameleon Cloud User <shixun404@gmail.com>
2025-02-06 07:39:03 +0000

    update

--- rapid/src/main/java/com/vrg/rapid/messaging/impl/GrpcClient.java
+++ rapid/src/main/java/com/vrg/rapid/messaging/impl/GrpcClient.java
@@ -145,7 +145,7 @@ public class GrpcClient implements IMessagingClient {
         
         // Generate a Gaussian value and scale it to mean and standard deviation
         final double gaussian = random.nextGaussian();
-        final double latency = Math.min(300, Math.max(meanLatency + gaussian * stdDevLatency, 5));
+        final double latency = Math.min(200, Math.max(meanLatency + gaussian * stdDevLatency, 5));
         // final double latency = Math.max(meanLatency + gaussian * stdDevLatency, 5);
         
         latencyCache.put(key, latency);
