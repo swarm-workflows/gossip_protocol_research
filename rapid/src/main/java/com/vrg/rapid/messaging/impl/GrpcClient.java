47bfb6921c353edaeaebcf2f34074ada70ec7ab5
Chameleon Cloud User <shixun404@gmail.com>
2025-02-06 08:12:34 +0000

    update

--- rapid/src/main/java/com/vrg/rapid/messaging/impl/GrpcClient.java
+++ rapid/src/main/java/com/vrg/rapid/messaging/impl/GrpcClient.java
@@ -73,7 +73,7 @@ public class GrpcClient implements IMessagingClient {
     public final Map<String, Double> latencyCache = new HashMap<>();
     public static final int DEFAULT_GRPC_TIMEOUT_MS = 1000;
     public static final int DEFAULT_GRPC_DEFAULT_RETRIES = 5;
-    public static final int DEFAULT_GRPC_JOIN_TIMEOUT = DEFAULT_GRPC_TIMEOUT_MS * 5;
+    public static final int DEFAULT_GRPC_JOIN_TIMEOUT = DEFAULT_GRPC_TIMEOUT_MS * 15;
     public static final int DEFAULT_GRPC_PROBE_TIMEOUT = 1000;
 
     private final Endpoint address;
