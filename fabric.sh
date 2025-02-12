5011606cd0eaa0931441f4c21c8b68c88eea9b63
Chameleon Cloud User <shixun404@gmail.com>
2025-02-12 21:41:23 +0000

    update

--- fabric.sh
+++ fabric.sh
@@ -8,3 +8,4 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 source ~/miniconda3/bin/activate
 conda create -n gossip python=3.12 -y
+sudo apt install firewalld -y
