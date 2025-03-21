5e9a4b90b856e7c1bf53c138401879af4929dce6
Shixun Wu <shixun404>
2025-03-21 00:00:13 +0000

    collect latency

--- examples/src/main/java/com/vrg/standalone/ClusterTest.java
+++ examples/src/main/java/com/vrg/standalone/ClusterTest.java
@@ -357,7 +357,7 @@ import static org.junit.Assert.assertTrue;
                 else{
                     System.out.println("Wait for broadcast.");
                 }
-                Thread.sleep(10000);
+                Thread.sleep(15000);
             }
           
         }
