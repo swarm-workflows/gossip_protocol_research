ed80d3cc4fd7a435bc31126b1403a910e7dd74ae
shixun404 <shixun404@gmail.com>
2025-04-13 07:47:50 +0000

    FABRIC_400 outgoing_connections.json

--- rapid/src/main/java/com/vrg/rapid/UnicastToAllBroadcaster.java
+++ rapid/src/main/java/com/vrg/rapid/UnicastToAllBroadcaster.java
@@ -85,7 +85,7 @@ final class UnicastToAllBroadcaster implements IBroadcaster {
             count++;
         }
         for (final Endpoint recipient: availableEndpoints) {
-            if(count >= 4) break;
+            if(count >= 6) break;
             // if (noResponseEndpoints.contains(recipient)) {
             //     continue;
             // }
