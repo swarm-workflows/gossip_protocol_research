fddad337a9c6cc3596635fdf42cfcb0756a4c0b6
shixun404 <shixun404@gmail.com>
2025-10-05 01:15:57 +0000

    update

--- install_bash.sh
+++ install_bash.sh
@@ -6,7 +6,7 @@ wget https://download.java.net/java/GA/jdk9/9.0.4/binaries/openjdk-9.0.4_linux-x
 tar -xzvf openjdk-9.0.4_linux-x64_bin.tar.gz -C $HOME/software
 rm -f openjdk-9.0.4_linux-x64_bin.tar.gz
 
-wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
+wget https://archive.apache.org/dist/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
 tar -xzvf apache-maven-3.9.6-bin.tar.gz -C $HOME/software
 rm -f apache-maven-3.9.6-bin.tar.gz
 
@@ -21,4 +21,4 @@ echo "export PATH=\$HOME/software/jdk-9.0.4/bin:\$HOME/software/apache-maven-3.9
 # export maven opts to allow insecure ssl
 export MAVEN_OPTS="-Dmaven.resolver.transport=wagon -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true"
 
-mvn $MAVEN_OPTS clean install
+mvn clean install -U -DfailIfNoTests=false -Dmaven.resolver.transport=wagon -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
